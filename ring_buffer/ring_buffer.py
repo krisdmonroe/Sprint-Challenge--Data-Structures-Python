3f3d6538419e168adce8cdf9d50b51259f4a0113
Kris Monroe <krisdmonroe@gmail.com>
2020-05-15 09:35:33 -0700

    Best i can do for now

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,26 +1,77 @@
-from reverse import DoublyLinkedList
+from reverse import LinkedList
 
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.current = None
-        self.storage = DoublyLinkedList()
+        self.storage = LinkedList()
 
     def append(self, item):
-        # front stays the same and items are added to the tail
-        # want to compare the length of storage to the capcaity of the ring
-        if self.storage.length < self.capacity:
-            # add item to the tail
-            self.storage.add_to_tail(item)
-            # set current to the tail
-            self.current = self.storage.tail
-            # if current and tail are the same we know we need to remove from the head and insert new item
-        elif self.current == self.storage.tail:
-            self.storage.remove_from_head()
-            self.storage.add_to_head(item)
-            # make current location the head
-            self.current = self.storage.head
-        
+        # # front stays the same and items are added to the tail
+        # # want to compare the length of storage to the capcaity of the ring
+        # if self.storage.length < self.capacity:
+        #     # add item to the tail
+        #     self.storage.add_to_tail(item)
+        #     self.current = self.storage.tail
+        # if self.storage.length == 1:
+        #     self.current = self.storage.head
+        # else:
+        #     if self.capacity > self.storage.length:
+        #         self.storage.add_to_tail(item)
 
+        #     elif self.current == self.storage.tail:
+        #         self.storage.remove_from_head()
+        #         self.storage.add_to_head(item)
+        #     # make current the head
+        #         self.current = self.storage.head
+        # Above not working going to try something else
+        # if self.storage.length < self.capacity:
+        #     self.storage.add_to_tail(item)
+        #     self.current = self.storage.tail
+        # else:
+        #     if self.current == self.storage.tail:
+        #         self.storage.remove_from_head()
+        #         self.storage.add_to_head(item)
+        #         self.current = self.storage.head
+        # if self.capacity > self.storage.length:
+        #     self.storage.add_to_tail(item)
+        #     self.current = self.storage.tail
+        # else:
+        #     if self.current != self.storage.tail:
+        #         self.current = self.storage.head
+        #     else:
+        #         self.current = self.storage.head
+        # if self.storage.length < self.capacity:
+        #     self.storage.add_to_tail(item)
+        # else:
+        #     if self.current == None:
+        #         self.current = self.storage.head
+        #     elif self.current.next == None:
+        #         self.current = self.storage.head
+        #     else:
+        #         self.current = self.current.next
+        #     self.current.value = item
+        if self.capacity < len(self.storage):
+            self.storage.add_to_end(item)
+            self.current = self.storage.head
+        else:
+            if self.capacity == len(self.storage):
+                self.storage.remove_from_head()
+                self.storage.add_to_end(item)
+                self.current = self.storage.head
+            else:
+                self.current.value = item
+
+            
+            
     def get(self):
-        pass
+        buffer = []
+        # makes a variable to make while loop go
+        node = self.storage.head
+        # while something
+        while node:
+            # add into buffer the value of the node
+            buffer.append(node.value)
+            # continue the loop
+            node = node.next
+        return buffer
