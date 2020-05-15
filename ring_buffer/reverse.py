de9b2b96b9f53a7232c052128bb750fbacbaea9f
Kris Monroe <krisdmonroe@gmail.com>
2020-05-14 23:27:11 -0700

    Progress??

--- ring_buffer/reverse.py
+++ ring_buffer/reverse.py
@@ -1,68 +1,172 @@
-class Node:
-    def __init__(self, value=None, next_node=None):
+"""Each ListNode holds a reference to its previous node
+as well as its next node in the List."""
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
         self.value = value
-        self.next_node = next_node
+        self.prev = prev
+        self.next = next
 
-    def get_value(self):
-        return self.value
+    """Wrap the given value in a ListNode and insert it
+    after this node. Note that this node could already
+    have a next node it is point to."""
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
 
-    def get_next(self):
-        return self.next_node
+    """Wrap the given value in a ListNode and insert it
+    before this node. Note that this node could already
+    have a previous node it is point to."""
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
 
-    def set_next(self, new_next):
-        self.next_node = new_next
+    """Rearranges this ListNode's previous and next pointers
+    accordingly, effectively deleting this ListNode."""
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
 
-    def add_to_head(self, value):
-        node = Node(value)
-
-        if self.head is not None:
-            node.set_next(self.head)
-
+"""Our doubly-linked list class. It holds references to
+the list's head and tail nodes."""
+class DoublyLinkedList:
+    def __init__(self, node=None):
         self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
 
-    def contains(self, value):
-        if not self.head:
-            return False
+    def __len__(self):
+        return self.length
 
-        current = self.head
-
-        while current:
-            if current.get_value() == value:
-                return True
-
-            current = current.get_next()
-
-        return False
-
-    def add_to_end(self, value):
-        # regardless of if the list is empty or not, we need to wrap the value in a Node 
-        new_node = Node(value)
-        # what if the list is empty? 
-        if not self.head:
+    """Wraps the given value in a ListNode and inserts it 
+    as the new head of the list. Don't forget to handle 
+    the old head node's previous pointer accordingly."""
+    def add_to_head(self, value):
+        new_node = ListNode(value)
+        self.length += 1
+        if not self.head and not self.tail:
             self.head = new_node
-        # what if the list isn't empty?
+            self.tail = new_node
         else:
-            # what node do we want to add the new node to? 
-            # the last node in the list 
-            # we can get to the last node in the list by traversing it 
-            current = self.head 
-            while current.get_next() is not None:
-                current = current.get_next()
-            # we're at the end of the linked list 
-            current.set_next(new_node)
-            
-    def reverse_list(self, node, prev):
-        if self.head is None:
-            return None
-        elif node.next_node:
-            new_node = node.get_next()
-            self.reverse_list(new_node, node)
-        else:
-            self.head = node
-        
-        node.set_next(prev)
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
 
+
+    """Removes the List's current head node, making the
+    current head's next node the new head of the List.
+    Returns the value of the removed Node."""
+    def remove_from_head(self):
+            # we want to return the value at the current head 
+        value = self.head.value
+            # remove the value at the head 
+            # update self.head 
+        self.delete(self.head)
+        return value
+
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new tail of the list. Don't forget to handle 
+    the old tail node's next pointer accordingly."""
+    def add_to_tail(self, value):
+        new_node = ListNode(value)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+
+    """Removes the List's current tail node, making the 
+    current tail's previous node the new tail of the List.
+    Returns the value of the removed Node."""
+    def remove_from_tail(self):
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new head node of the List."""
+    def move_to_front(self, node):
+        if node is self.head:
+            return
+        value = node.value
+        if node is self.tail:
+            self.remove_from_tail()
+        else:
+            node.delete()
+            self.length -= 1
+        self.add_to_head(value)
+
+            
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new tail node of the List."""
+    def move_to_end(self, node):
+        # already at the tail so return
+        if node is self.tail:
+            return
+        value = node.value
+        # move the head to the tail
+        if node is self.head:
+            self.remove_from_head()
+            self.add_to_tail(value)
+        else:
+            node.delete()
+            self.length -= 1
+            self.add_to_tail(value)
+            
+
+    """Removes a node from the list and handles cases where
+    the node was the head or the tail"""
+    def delete(self, node):
+        # since we are removing change the length of the list
+        self.length -= 1
+        # if there is no head and no tail
+        if not self.head and not self.tail:
+            return 
+        # head and tail are the same means one node
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        # if the node is the head do this
+        elif self.head == node:
+            # set what your about to delete to the next in line since were at the head
+            self.head = node.next
+            # delete the node after 
+            node.delete()
+        # if the node is the tail do this
+        elif self.tail == node:
+            # since were at the tail set new tail to be the previous since were on tail
+            self.tail = node.prev
+            # now delete that node
+            node.delete()
+        else:
+            # if not caught by any other if statments delete the node
+            node.delete()
+        
+
+    """Returns the highest value currently in the list"""
+    def get_max(self):
+        if not self.head:
+            return None
+        # go through the list and find the largest value
+        # set a max value and set a current value your searching with
+        max_value = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            # find a way to continue the search
+            current = current.next
+            # should search through everything and then return the max value
+        return max_value
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,13 +1,26 @@
-from reverse import LinkedList
+from reverse import DoublyLinkedList
 
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.size = 0
-        self.storage = LinkedList()
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        self.storage.add_to_end(item)
-        self.size += 1
+        # front stays the same and items are added to the tail
+        # want to compare the length of storage to the capcaity of the ring
+        if self.storage.length < self.capacity:
+            # add item to the tail
+            self.storage.add_to_tail(item)
+            # set current to the tail
+            self.current = self.storage.tail
+            # if current and tail are the same we know we need to remove from the head and insert new item
+        elif self.current == self.storage.tail:
+            self.storage.remove_from_head()
+            self.storage.add_to_head(item)
+            # make current location the head
+            self.current = self.storage.head
+        
+
     def get(self):
         pass
